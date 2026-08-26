3dc36bc72d4496498a3eabf739d90ffd02e86a23
yoselin55 <yoselin.flores@tecsup.edu.pe>
2026-08-26 02:37:21 -0500

    "Proyecto inicial con menú del  carrito"

---
+++ Semana02/lab-manual02/app/src/main/java/com/flores/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.flores.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
