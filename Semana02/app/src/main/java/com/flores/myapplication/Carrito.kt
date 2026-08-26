f5ccbf86c6356ac6eb7c8fdfcb0297180f57f94f
yoselin55 <yoselin.flores@tecsup.edu.pe>
2026-08-26 02:02:45 -0500

    “Proyecto inicial con menú del carrito”

---
+++ Semana02/app/src/main/java/com/flores/myapplication/Carrito.kt
@@ -0,0 +1,13 @@
+package com.flores.myapplication
+
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
