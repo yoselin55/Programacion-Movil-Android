26f47cecfa3cfc097918231cffc14b28055033ed
Yoselin Flores <yoselin.flores@tecsup.edu.pe>
2026-09-23 10:35:12 -0500

    integrar AppNavigation en MainActivity y completar Fase 1

--- Semana06/TECSUP Fit/app/src/main/java/com/flores/tecsupfit/MainActivity.kt
+++ Semana06/TECSUP Fit/app/src/main/java/com/flores/tecsupfit/MainActivity.kt
@@ -1,47 +1,21 @@
-package com.flores.tecsupfit
+package com.example.semana05_navegacion
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.flores.tecsupfit.ui.theme.TECSUPFitTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.semana05_navegacion.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TECSUPFitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface {
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TECSUPFitTheme {
-        Greeting("Android")
     }
 }
