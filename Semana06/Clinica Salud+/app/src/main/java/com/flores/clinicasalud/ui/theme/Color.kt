15c0f85869582980d44e27295f6205b8f655c173
Yoselin Flores <yoselin.flores@tecsup.edu.pe>
2026-09-23 06:51:35 -0500

    definir paleta de colores y tema inicial de la aplicacion

--- Semana06/Clinica Salud+/app/src/main/java/com/flores/clinicasalud/ui/theme/Color.kt
+++ Semana06/Clinica Salud+/app/src/main/java/com/flores/clinicasalud/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.flores.clinicasalud.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryPurple = Color(0xFF5B2E91)
+val HeaderPurple = Color(0xFF59278E)
+val LightPurpleCard = Color(0xFFF3EDF7)
+val ChipUnselected = Color(0xFFEFECEF)
+val TextDark = Color(0xFF1C1B1F)
+val TextMuted = Color(0xFF67626D)
+val SuccessGreen = Color(0xFF34A853)
+val SuccessGreenBg = Color(0xFFE6F4EA)
+val CompletedGrayBg = Color(0xFFEEEEEE)
+val CompletedGrayText = Color(0xFF616161)
+val BackgroundWhite = Color(0xFFFAFAFA)
