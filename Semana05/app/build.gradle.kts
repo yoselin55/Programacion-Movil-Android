c13b4907c372b223bacdf983454a3cb02d454afb
Yoselin Flores <yoselin.flores@tecsup.edu.pe>
2026-09-22 22:51:07 -0500

    agregar dependencia de iconos extendidos e implementar ListScreen

--- Semana05/app/build.gradle.kts
+++ Semana05/app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended") //Dependencia de íconos extendidos
 }
